2e78e296efe8a09262b0786e6a184887c576cc4c
Brian Hoang <brian.hd.hoang@mintecglobal.com>
2024-09-19 14:56:17 +0100

    refactor tests

--- src/payments/payments.controller.spec.ts
+++ src/payments/payments.controller.spec.ts
@@ -11,63 +11,67 @@ describe('PaymentsController', () => {
     paymentsController = new PaymentsController(paymentsService);
   });
 
-  it('should return all payments', () => {
-    const allPayments = [
-      {
+  describe('getAllPayments', () => {
+    it('should return all payments', () => {
+      const allPayments = [
+        {
+          amount: 100,
+          status: 'initialised' as const,
+          product: testProduct,
+          paymentMethod: testPaymentMethod,
+          user: testUser,
+        },
+      ];
+
+      jest
+        .spyOn(paymentsService, 'findAll')
+        .mockImplementation(() => allPayments);
+
+      const expected = {
+        payments: allPayments,
+      };
+
+      expect(paymentsController.getAllPayments({})).toStrictEqual(expected);
+    });
+
+    it('should return all payments of the specified status', () => {
+      const completePayment = {
         amount: 100,
-        status: 'initialised' as const,
+        status: 'complete' as const,
         product: testProduct,
         paymentMethod: testPaymentMethod,
         user: testUser,
-      },
-    ];
+      };
 
-    jest
-      .spyOn(paymentsService, 'findAll')
-      .mockImplementation(() => allPayments);
+      const filteredPayments = [completePayment];
 
-    const expected = {
-      payments: allPayments,
-    };
+      const query: GetAllPaymentsQuery = { status: 'complete' as const };
 
-    expect(paymentsController.getAllPayments({})).toStrictEqual(expected);
+      const findAll = jest
+        .spyOn(paymentsService, 'findAll')
+        .mockImplementation(() => filteredPayments);
+
+      const expected = {
+        payments: filteredPayments,
+      };
+
+      expect(paymentsController.getAllPayments(query)).toStrictEqual(expected);
+
+      expect(findAll).toBeCalledWith(query.status);
+    });
   });
 
-  it('should return all payments of the specified status', () => {
-    const completePayment = {
-      amount: 100,
-      status: 'complete' as const,
-      product: testProduct,
-      paymentMethod: testPaymentMethod,
-      user: testUser,
-    };
+  describe('getTotalForCompletedPayments', () => {
+    it('should return total for completed payments', () => {
+      const total = 100;
 
-    const filteredPayments = [completePayment];
+      const findAll = jest
+        .spyOn(paymentsService, 'getCompletedPaymentsTotal')
+        .mockReturnValue(total);
 
-    const query: GetAllPaymentsQuery = { status: 'complete' as const };
-
-    const findAll = jest
-      .spyOn(paymentsService, 'findAll')
-      .mockImplementation(() => filteredPayments);
-
-    const expected = {
-      payments: filteredPayments,
-    };
-
-    expect(paymentsController.getAllPayments(query)).toStrictEqual(expected);
-
-    expect(findAll).toBeCalledWith(query.status);
-  });
-
-  it('should return total for completed payments', () => {
-    const total = 100;
-
-    const findAll = jest
-      .spyOn(paymentsService, 'getCompletedPaymentsTotal')
-      .mockReturnValue(total);
-
-    expect(paymentsController.getTotalForCompletedPayments()).toEqual({
-      total,
+      expect(paymentsController.getTotalForCompletedPayments()).toEqual({
+        total,
+      });
     });
   });
 });
