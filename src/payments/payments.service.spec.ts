a6556ad3a72b4b76dc3f8540be7048c9e6b3dea3
Brian Hoang <brian.hd.hoang@mintecglobal.com>
2024-09-19 11:38:35 +0100

    create payment service

---
+++ src/payments/payments.service.spec.ts
@@ -0,0 +1,46 @@
+﻿import { Test, TestingModule } from '@nestjs/testing';
+import { Payment } from '../interfaces/payment.interface';
+import { PaymentsService } from './payments.service';
+
+const testUser = {
+  name: 'Bobby tables',
+  email: 'bobby@tables.com',
+};
+
+const testPaymentMethod = {
+  user: testUser,
+  cardNumber: 555,
+};
+
+const testProduct = {
+  name: 'new product',
+  description: 'test',
+  price: 11.99,
+  stockLevel: 0,
+};
+
+describe('PaymentsService', () => {
+  let service: PaymentsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PaymentsService],
+    }).compile();
+
+    service = module.get<PaymentsService>(PaymentsService);
+  });
+
+  it('adds and retrieves payments to the store', () => {
+    const newPayment: Payment = {
+      amount: 100,
+      status: 'initialised',
+      product: testProduct,
+      paymentMethod: testPaymentMethod,
+      user: testUser,
+    };
+
+    service.addPayment(newPayment);
+
+    expect(service.findAll()).toEqual([newPayment]);
+  });
+});
---
+++ src/payments/payments.service.ts
@@ -0,0 +1,15 @@
+﻿import { Injectable } from '@nestjs/common';
+import { Payment } from '../interfaces/payment.interface';
+
+@Injectable()
+export class PaymentsService {
+  private readonly payments: Payment[] = [];
+
+  addPayment(payment: Payment) {
+    this.payments.push(payment);
+  }
+
+  findAll(): Payment[] {
+    return this.payments;
+  }
+}
