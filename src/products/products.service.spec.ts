4734a92be66c7e09e99cde153a26ee1ef6be1ee7
Brian Hoang <brian.hd.hoang@mintecglobal.com>
2024-09-19 11:12:59 +0100

    create products service

---
+++ src/products/products.service.spec.ts
@@ -0,0 +1,27 @@
+﻿import { Test, TestingModule } from '@nestjs/testing';
+import { ProductsService } from './products.service';
+
+describe('ProductsService', () => {
+  let service: ProductsService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ProductsService],
+    }).compile();
+
+    service = module.get<ProductsService>(ProductsService);
+  });
+
+  it('adds and retrieves products to the store', () => {
+    const newProduct = {
+      name: 'new product',
+      description: 'new description',
+      price: 11.99,
+      stockLevel: 1,
+    };
+
+    service.addProduct(newProduct);
+
+    expect(service.findAll()).toEqual([newProduct]);
+  });
+});
---
+++ src/products/products.service.ts
@@ -0,0 +1,15 @@
+﻿import { Injectable } from '@nestjs/common';
+import { Product } from '../interfaces/product.interface';
+
+@Injectable()
+export class ProductsService {
+  private readonly products: Product[] = [];
+
+  addProduct(product: Product) {
+    this.products.push(product);
+  }
+
+  findAll(): Product[] {
+    return this.products;
+  }
+}
